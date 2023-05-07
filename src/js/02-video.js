819032394e18a277b2f0daeb470af88b560138d2
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-07 13:33:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
-import Vimeo from "@vimeo/Player";
-import trottle from "lodash.throttle";
-const iframe = document.querySelector("iframe");
+import Vimeo from '@vimeo/player';
+import trottle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-const storageKey = "videoplayer-current-time";
-player.on("play", function () {
-  console.log("played the video!");
+const storageKey = 'videoplayer-current-time';
+player.on('play', function () {
+  console.log('played the video!');
 });
 
-player.on("timeupdate", trottle(setTimeToLocale, 1000));
+player.on('timeupdate', trottle(setTimeToLocale, 1000));
 function setTimeToLocale(currentTime) {
   const seconds = currentTime.seconds;
   localStorage.setItem(storageKey, JSON.stringify(seconds));
