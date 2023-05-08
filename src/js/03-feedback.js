2193d7255261406eaafd7aa8979d7a4555f25cf6
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-08 14:49:30 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,22 @@
-import trottle from "lodash.throttle";
+import trottle from 'lodash.throttle';
 
-const email = document.querySelector(".email");
-const message = document.querySelector(".message");
-const feedBackForm = document.querySelector(".feedback-form");
-const storageKey = "feedback-form-state";
+const email = document.querySelector('.email');
+const message = document.querySelector('.message');
+const feedBackForm = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+let userData = {
+  email: '',
+  message: '',
+};
+
+email &&
+  message.addEventListener('input', () => {
+    userData = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem(storageKey, JSON.stringify(userData));
+  });
 
 if (localStorage.getItem(storageKey) !== null) {
   let data = JSON.parse(localStorage.getItem(storageKey));
@@ -13,13 +26,9 @@ if (localStorage.getItem(storageKey) !== null) {
 
 function formHandlerInput(event) {
   event.preventDefault();
-  let userData = {
-    email: email.value,
-    message: message.value,
-  };
-  email.value = "";
-  message.value = "";
+  email.value = '';
+  message.value = '';
+  console.log(localStorage.getItem(storageKey));
   localStorage.clear();
-  localStorage.setItem(storageKey, JSON.stringify(userData));
 }
-feedBackForm.addEventListener("submit", trottle(formHandlerInput, 500));
+feedBackForm.addEventListener('submit', trottle(formHandlerInput, 500));
