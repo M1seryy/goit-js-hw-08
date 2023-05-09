18bae40a32ca6fdfb995fa84f25052937822939b
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-09 12:50:23 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,32 +3,35 @@ import trottle from 'lodash.throttle';
 const email = document.querySelector('.email');
 const message = document.querySelector('.message');
 const feedBackForm = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
-let userData = {
-  email: '',
-  message: '',
-};
+const storageKeyEmail = 'feedback-form-state-email';
+const storageKeyMessage = 'feedback-form-state-message';
 
-email &&
-  message.addEventListener('input', () => {
-    userData = {
-      email: email.value,
-      message: message.value,
-    };
-    localStorage.setItem(storageKey, JSON.stringify(userData));
-  });
+message.addEventListener('input', () => {
+  localStorage.setItem(storageKeyMessage, JSON.stringify(message.value));
+});
+email.addEventListener('input', () => {
+  localStorage.setItem(storageKeyEmail, JSON.stringify(email.value));
+});
 
-if (localStorage.getItem(storageKey) !== null) {
-  let data = JSON.parse(localStorage.getItem(storageKey));
-  email.value = data.email;
-  message.value = data.message;
+if (
+  localStorage.getItem(storageKeyEmail) &&
+  localStorage.getItem(storageKeyMessage) !== null
+) {
+  let dataEmail = JSON.parse(localStorage.getItem(storageKeyEmail));
+  let dataMessage = JSON.parse(localStorage.getItem(storageKeyMessage));
+  email.value = dataEmail;
+  message.value = dataMessage;
 }
 
 function formHandlerInput(event) {
   event.preventDefault();
   email.value = '';
   message.value = '';
-  console.log(localStorage.getItem(storageKey));
-  localStorage.clear();
+  console.log({
+    email: JSON.parse(localStorage.getItem(storageKeyEmail)),
+    message: JSON.parse(localStorage.getItem(storageKeyMessage)),
+  });
+  localStorage.removeItem(storageKeyEmail);
+  localStorage.removeItem(storageKeyMessage);
 }
 feedBackForm.addEventListener('submit', trottle(formHandlerInput, 500));
