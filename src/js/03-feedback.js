f920d0a9329c8a6306485a3cfe49f7edf9bf3571
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-05-10 10:58:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,35 +3,39 @@ import trottle from 'lodash.throttle';
 const email = document.querySelector('.email');
 const message = document.querySelector('.message');
 const feedBackForm = document.querySelector('.feedback-form');
-const storageKeyEmail = 'feedback-form-state-email';
-const storageKeyMessage = 'feedback-form-state-message';
+const storageKey = 'feedback-form-state';
+
+email.addEventListener('input', () => {
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.removeItem(storageKey);
+  localStorage.setItem(storageKey, JSON.stringify(userData));
+});
 
 message.addEventListener('input', () => {
-  localStorage.setItem(storageKeyMessage, JSON.stringify(message.value));
-});
-email.addEventListener('input', () => {
-  localStorage.setItem(storageKeyEmail, JSON.stringify(email.value));
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.removeItem(storageKey);
+  localStorage.setItem(storageKey, JSON.stringify(userData));
 });
 
-if (
-  localStorage.getItem(storageKeyEmail) &&
-  localStorage.getItem(storageKeyMessage) !== null
-) {
-  let dataEmail = JSON.parse(localStorage.getItem(storageKeyEmail));
-  let dataMessage = JSON.parse(localStorage.getItem(storageKeyMessage));
-  email.value = dataEmail;
-  message.value = dataMessage;
+if (localStorage.getItem(storageKey) !== null) {
+  let dataUser = JSON.parse(localStorage.getItem(storageKey));
+
+  email.value = dataUser.email;
+  message.value = dataUser.message;
 }
 
 function formHandlerInput(event) {
   event.preventDefault();
   email.value = '';
   message.value = '';
-  console.log({
-    email: JSON.parse(localStorage.getItem(storageKeyEmail)),
-    message: JSON.parse(localStorage.getItem(storageKeyMessage)),
-  });
-  localStorage.removeItem(storageKeyEmail);
-  localStorage.removeItem(storageKeyMessage);
+  console.log(JSON.parse(localStorage.getItem(storageKey)));
+  localStorage.removeItem(storageKey);
 }
 feedBackForm.addEventListener('submit', trottle(formHandlerInput, 500));
